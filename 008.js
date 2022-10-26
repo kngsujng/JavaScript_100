746c06a4eaa3e52680d0f9a730e09683e7b440ce
kngsujng <kngsujng@naver.com>
2022-10-26 23:48:14 +0900

    10번까지 문제풀이

---
+++ 008.js
@@ -0,0 +1,15 @@
+// 문제 8 : 객체의 키 이름 중복 
+// 출력값을 입력하시오. (출력값은 공백을 넣지 않는다.)
+
+var d = {
+    'height':180,
+    'weight':78,
+    'weight':84,
+    'temperature':36,
+    'eyesight':1
+};
+
+console.log(d['weight']);
+
+//풀이 
+// 84
